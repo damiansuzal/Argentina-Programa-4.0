925b24e2761ff4b2f8d51afc26ae232fa94a3f87
Damian <67932123+damiansuzal@users.noreply.github.com>
2023-09-26 21:35:22 -0300

    Delete Unidad 2 directory

--- Unidad 2/Funciones Ejercicio 10.py
+++
@@ -1,9 +0,0 @@
-"""Definir una función inversa() que calcule la inversión de una cadena. 
-Por ejemplo la cadena "estoy probando" debería devolver la cadena "odnaborp yotse"""
-def invertido(cad):
-    return cad[::-1]
-
-# devuelve el string con los caracteres en orden contrario a como están escritos
-cadena= input(str("Ingrese una cadena para invertir: "))
-invertida= invertido(cadena)
-print('La cadena invertida es: ','"', invertida, '"')
